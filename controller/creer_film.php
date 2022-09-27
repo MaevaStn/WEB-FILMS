3ac5257226093e1e212941454d0d95618e6de864
Elise Lpc <liselaplanche@gmx.fr>
2022-09-27 15:47:29 +0200

    essai 2

--- controller/creer_film.php
+++ controller/creer_film.php
@@ -1,9 +1,1 @@
 <?php
-$filmsDao = new FilmsDAO();
-// * @var $alloffers type */
-//  va tous chercher et stock ds un tableau :
-$allfilms = $filmsDao->getAll();
-
-print_r($allfilms);
-// On affiche le template Twig correspondant
-echo $twig->render('creer_film.html.twig', ['allfilms' => $allfilms]);
--- controller/film.php
+++ controller/film.php
@@ -1,13 +1,9 @@
 <?php
 $filmsDAO = new FilmsDAO();
-//  va tous chercher et stock ds un tableau :
-$allfilms = $filmsDAO->getAll();
-print_r($allfilms);
 
-// On affiche le template Twig correspondant
+if (isset($_POST["search"])) {;
+    $allfilms = $filmsDAO->getAll($_POST['search']);
+} else {
+    $allfilms = $filmsDAO->getAll('');
+}
 echo $twig->render('film.html.twig', ['allfilms' => $allfilms]);
-
-// crea form pour récup resultat de la recherche via bouton rechercher
-// chame de recherche via son name 
-// recup ds variable
-// isset get all ou sinon  get av le recherche
