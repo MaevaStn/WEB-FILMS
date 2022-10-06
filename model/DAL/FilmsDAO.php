bca31eb3b2db405a6ed5d06a4c98e3367fac0240
Elise Lpc <liselaplanche@gmx.fr>
2022-10-06 16:12:02 +0200

    essai

--- model/DAL/FilmsDAO.php
+++ model/DAL/FilmsDAO.php
@@ -6,7 +6,7 @@ class FilmsDAO extends Dao
     //Récupérer :
     public function getAll($recherche)
     {
-        //SQL : SELECT * FROM films INNER JOIN role INNER JOIN acteurs ON acteurs.idActeur = role.idActeur WHERE role.idFilm = films.idFilm AND UPPER(titre) LIKE UPPER('M%')
+        //SQL : SELECT * FROM films INNER JOIN role INNER JOIN acteurs ON acteurs.idActeur = role.idActeur WHERE role.idFilm = films.idFilm AND UPPER(titre) LIKE UPPER('m%')
         // définir BDD :
         $queryFilms = $this->_bdd->prepare("SELECT * FROM films INNER JOIN role ON role.idFilm = films.idFilm  INNER JOIN acteurs ON acteurs.idActeur = role.idActeur WHERE UPPER(titre) LIKE UPPER(:recherche) ORDER BY films.idFilm ASC");
         // permet de rechercher toutes les lignes de “colonne” qui commence par (une lettre définie) / mais dans notre cas, en fonction de ($recherche) :
@@ -14,7 +14,10 @@ class FilmsDAO extends Dao
         $tabfilms = array();
 
         $idFilm = 0;
+        // parcours du resultat de la requete ligne apres ligne :
+        // sachant qu'avec le order by les films se suivent les uns apres les autres.
         while ($dataFilms = $queryFilms->fetch()) {
+            // detection chg film par idfilm :
             if ($idFilm != $dataFilms['idFilm']) {
                 $film = new Film($dataFilms['idFilm'], $dataFilms['titre'], $dataFilms["realisateur"], $dataFilms['affiche'], $dataFilms['annee'], null);
                 $acteur = new Acteur($dataFilms["idActeur"], $dataFilms["nom"], $dataFilms["prenom"]);
@@ -23,16 +26,16 @@ class FilmsDAO extends Dao
                 $film->addRole($role);
                 $tabfilms[] = $film;
             } else {
-                // array_key_last — Récupère la dernière clé d'un tableau
+                // array_key_last — Récupère la dernière clé d'un tableau, 
+                // récupèere le dernier film stocké dans $tabFilm ds le but de lui ajouter un role et un acteur
                 $film = $tabfilms[array_key_last($tabfilms)];
+                var_dump($film);
                 $acteur = new Acteur($dataFilms["idActeur"], $dataFilms["nom"], $dataFilms["prenom"]);
                 $role = new Role($acteur, $dataFilms["personnage"], $dataFilms["idRole"]);
                 $film->addRole($role);
             }
             $idFilm = $dataFilms['idFilm'];
-            // print_r($dataFilms);
-            // // var_dump($films);
-
+            // var_dump($idFilm);
         }
         return $tabfilms;
     }
